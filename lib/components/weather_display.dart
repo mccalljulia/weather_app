d0bec1decac7bd158325ae353d4e968726d60053
Julia McCall <mccall@student.chalmers.se>
2025-08-06 16:10:43 +0200

    #9 Add some icons (remaining icons in progress)

--- lib/components/weather_display.dart
+++ lib/components/weather_display.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import '../models/weather.dart';
+import 'package:intl/intl.dart';
 
 class WeatherDisplay extends StatelessWidget {
   final Weather weather;
-  const WeatherDisplay({super.key, required this.weather});
+  final dateFormat = DateFormat('EEEE, MMM d, yyyy');
+  final timeFormat = DateFormat('hh:mm a');
+
+  WeatherDisplay({super.key, required this.weather});
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +17,12 @@ class WeatherDisplay extends StatelessWidget {
           weather.city,
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
+        Text(dateFormat.format(weather.today)),
         Text(weather.description, style: TextStyle(fontSize: 18)),
         Text('${weather.temperature} Celsius', style: TextStyle(fontSize: 24)),
+        SizedBox(height: 20),
+        Text('Sunrise: ${timeFormat.format(weather.sunrise)}'),
+        Text('Sunset: ${timeFormat.format(weather.sunset)}'),
       ],
     );
   }
