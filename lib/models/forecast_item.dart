f24ac828f5ce913b447dc5f80564d8e5a6847316
Julia McCall <mccall@student.chalmers.se>
2025-08-08 12:47:57 +0200

    #3 Add icon mapping by icon code

--- lib/models/forecast_item.dart
+++ lib/models/forecast_item.dart
@@ -1,12 +1,27 @@
 class ForecastItem {
   final DateTime dateTime;
   final double temperature;
-  ForecastItem({required this.dateTime, required this.temperature});
+  final String description;
+  final String iconCode;
+
+  ForecastItem({
+    required this.dateTime,
+    required this.temperature,
+    required this.description,
+    required this.iconCode,
+  });
 
   static List<ForecastItem> fromJsonList(List<dynamic> list) {
-    return list.map((item) => ForecastItem(
-      dateTime: DateTime.fromMillisecondsSinceEpoch(item['dt'] * 1000),
-      temperature: (item['main']['temp'] as num).toDouble(),
-    )).toList();
+    final forecastList = list
+        .map(
+          (item) => ForecastItem(
+            dateTime: DateTime.fromMillisecondsSinceEpoch(item['dt'] * 1000),
+            temperature: (item['main']['temp'] as num).toDouble(),
+            description: item['weather'][0]['description'],
+            iconCode: item['weather'][0]['icon'],
+          ),
+        )
+        .toList();
+    return forecastList;
   }
 }
